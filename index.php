d3a56cad980eb2e395ce5a161d0c3c83acaaca49
LeandroT04 <leandrootoapanta@gmail.com>
2024-07-16 10:59:58 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
-session_start();
-
-$products = [
-    1 => ["name" => "Producto 1", "price" => 10],
-    2 => ["name" => "Producto 2", "price" => 20],
-    3 => ["name" => "Producto 3", "price" => 30],
-];
+require_once 'Product.php';
+$product = new Product();
+$products = $product->getProducts();
 ?>
 
 <!DOCTYPE html>
@@ -17,12 +13,12 @@ $products = [
 <body>
     <h1>Productos Disponibles</h1>
     <div class="products">
-        <?php foreach ($products as $id => $product) { ?>
+        <?php foreach ($products as $product) { ?>
             <div class="product">
                 <h2><?php echo $product["name"]; ?></h2>
                 <p>Precio: $<?php echo $product["price"]; ?></p>
                 <form action="add_to_cart.php" method="post">
-                    <input type="hidden" name="product_id" value="<?php echo $id; ?>">
+                    <input type="hidden" name="product_id" value="<?php echo $product["id"]; ?>">
                     <button type="submit">Añadir al Carrito</button>
                 </form>
             </div>
@@ -31,3 +27,5 @@ $products = [
     <a href="cart.php">Ver Carrito</a>
 </body>
 </html>
+
+
